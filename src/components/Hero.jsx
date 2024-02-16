7a1d085461328620240d77794fac4617da2a8964
TonyTech-GIT <toomgbebu@gmail.com>
2024-02-16 16:12:48 +0100

    changes on the page layout and mobile responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,11 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
+import GithubIcon from '../../public/assets/github.png'
+import InstagramIcon from '../../public/assets/instagram-3.png'
+import LinkedInIcon from '../../public/assets/linkedIn.png'
+
+
 import person from "../../public/assets/person.jpg"
 
 const Hero = () => {
@@ -12,9 +17,7 @@ const Hero = () => {
                     src={person}
                     effect='blur'
                     alt='person' />
-                {/* <div className="hero_container-img">
 
-                </div> */}
 
                 <div className="hero_container-text">
                     <h1>Hi, My name is Anthony, i specialize in making websites and webpages amazing, unique and everything in between.</h1>
@@ -25,6 +28,21 @@ const Hero = () => {
 
                     </div>
                 </div>
+
+
+            </div>
+            <div className='hero_container-socials'>
+                <a href="https://github.com/TonyTech-GIT">
+                    <img className='icon github' src={GithubIcon} alt="github-icon" />
+                </a>
+
+                <a href="https://www.instagram.com/mgbebu_anthony/"><img href="#" className='icon instagram' src={InstagramIcon} alt="instagram-icon" /></a>
+
+                <a href="https://www.linkedin.com/in/anthony-m-aa725b298/">
+                    <img className='icon linkedin' src={LinkedInIcon} alt="linkedin-icon" />
+                </a>
+
+
             </div>
         </section>
     )
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,18 +2,110 @@ import html from '../../public/assets/html.png'
 import css from '../../public/assets/css1.png'
 import js from '../../public/assets/js.png'
 import react from '../../public/assets/react.png'
+import figma from '../../public/assets/figma-1.png'
+import { useState } from 'react'
 
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+// import { LazyLoadImage } from 'react-lazy-load-image-component';
+// import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Skills = () => {
+    const [htmlValue, setHtmlValue] = useState(95)
+    const [cssValue, setCssValue] = useState(95)
+    const [jsValue, setJsValue] = useState(80)
+    const [reactValue, setReactValue] = useState(80)
+    const [figmaValue, setFigmaValue] = useState(70)
     return (
         <section className="skills" id="skills">
             <div className="skills_container">
                 <h1>Skills</h1>
 
                 <div className="skills_container-card-wrapper flex">
-                    <div className="skills_container-card">
+                    <div className="skills_container-subOne">
+                        <div className="skills_container-item">
+                            <img className='html-img' src={html} alt="html" />
+
+                            <input
+                                className='html-range'
+                                type="range"
+                                min="0"
+                                value={htmlValue}
+                                max="100"
+                                disabled
+                                onChange={(e) => setHtmlValue(e.target.value)}
+                            />
+                            <span className='html-text'>{htmlValue}%</span>
+                        </div>
+
+                        <div className="skills_container-item">
+                            <img className='css-img' src={css} alt="css" />
+
+                            <input
+                                className='css-range'
+                                type="range"
+                                min="0"
+                                value={cssValue}
+                                max="100"
+                                disabled
+                                onChange={(e) => setCssValue(e.target.value)}
+
+                            />
+                            <span className='css-text'>{cssValue}%</span>
+                        </div>
+
+                        <div className="skills_container-item">
+                            <img className='js-img' src={js} alt="js" />
+
+                            <input
+                                className='js-range'
+                                type="range"
+                                min="0"
+                                value={jsValue}
+                                max="100"
+                                disabled
+                                onChange={(e) => setJsValue(e.target.value)}
+
+                            />
+                            <span className='js-text'>{jsValue}%</span>
+                        </div>
+
+                        <div className="skills_container-item">
+                            <img className='react-img' src={react} alt="react" />
+
+                            <input
+                                className='react-range'
+                                type="range"
+                                min="0"
+                                value={reactValue}
+                                max="100"
+                                disabled
+                                onChange={(e) => setReactValue(e.target.value)}
+
+                            />
+                            <span className='react-text'>{reactValue}%</span>
+                        </div>
+                    </div>
+                    {/* Second skills container... */}
+                    <div className="skills_container-subTwo">
+                        <div className="skills_container-item">
+                            <img className='figma-img' src={figma} alt="figma" />
+
+                            <input
+                                className='figma-range'
+                                type="range"
+                                min="0"
+                                value={figmaValue}
+                                max="100"
+                                disabled
+                                onChange={(e) => setFigmaValue(e.target.value)}
+
+                            />
+                            <span className='figma-text'>{figmaValue}%</span>
+                        </div>
+                    </div>
+
+
+
+                    {/* <div className="skills_container-card">
 
                         <LazyLoadImage className='html-img' src={html}
                             effect='blur'
@@ -34,9 +126,7 @@ const Skills = () => {
                     </div>
 
                     <div className="skills_container-card">
-                        {/* <div className="skills_container-card-img">
                         
-                    </div> */}
                         <LazyLoadImage className='js-img' src={js}
                             effect='blur'
                             alt="js" />
@@ -54,7 +144,7 @@ const Skills = () => {
 
                         <h3 className="react-text">REACT</h3>
 
-                    </div>
+                    </div> */}
                 </div>
 
             </div>
