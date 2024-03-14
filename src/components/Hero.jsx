26edc15f044f9874bbd329817b8754e19e878033
TonyTech-GIT <toomgbebu@gmail.com>
2024-03-14 16:26:01 +0100

    Adjustments on the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { LazyLoadImage } from 'react-lazy-load-image-component';
+// import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 import GithubIcon from '../../public/assets/github.png'
@@ -6,22 +6,24 @@ import InstagramIcon from '../../public/assets/instagram-3.png'
 import LinkedInIcon from '../../public/assets/linkedIn.png'
 
 
-import person from "../../public/assets/person.jpg"
+// import person from "../../public/assets/person-2.jpg"
 
 const Hero = () => {
 
     return (
         <section className="hero" id='hero'>
             <div className="hero_container flex">
-                <LazyLoadImage className="hero_container-img"
+                {/* <LazyLoadImage className="hero_container-img"
                     src={person}
                     effect='blur'
-                    alt='person' />
+                    alt='person' /> */}
+
+                <div className="hero_container-img"></div>
 
 
                 <div className="hero_container-text">
-                    <h1>Hi, My name is Anthony, i specialize in making websites and webpages amazing, unique and everything in between.</h1>
-                    <p>I look forward to working with you.</p>
+                    <h1>Hi, My name is Anthony, am a skilled tech freelancer with specialties in creating websites, designing webpages, and everything in between.</h1>
+                    <p>I look forward to working with you. You are one click away.</p>
                     <div className="cta_container flex">
                         <a href="#contact" className="hire">Hire</a>
                         <a href="#works" className="know-more" >Know More</a>
